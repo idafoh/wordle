f00c9e9a2d7c955ba4e322a9f44b399f1f1dbd95
Abat Dauletbaev <abat.dauletbayev@gmail.com>
2022-11-03 14:11:18 +0500

    fix db path

--- server/api/db.get.ts
+++ server/api/db.get.ts
@@ -1,8 +1,12 @@
 import fs from 'node:fs'
 import path from 'node:path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 import { sendStream } from 'h3'
 
-export const dbPath = path.join(process.cwd(), 'db.sqlite3')
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+export const dbPath = path.resolve(__dirname, '..', '..', 'db.sqlite3')
 
 export default defineEventHandler((event) => {
   return sendStream(event, fs.createReadStream(dbPath))
